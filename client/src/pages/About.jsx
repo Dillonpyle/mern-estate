92c29968baf2f37b5238255d2b4e1a4a62c0995f
Dillonpyle <dillonpyle1991@gmail.com>
2024-12-29 17:54:48 -0600

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className='py-20 px-4 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-slate-800'>About <span className='text-slate-500'>Nest</span><span className='text-slate-700'>Quest</span></h1>
+            <p className='mb-4 text-slate-700'>
+            Welcome to Nest Quest, where buying, renting, or selling properties has never been easier! Our platform is designed to connect clients with their dream homes or help them achieve their real estate goals effortlessly. With intuitive search tools, detailed property listings, and personalized recommendations, we simplify the process to save you time and stress. Whether you’re a first-time buyer, a seasoned investor, or someone looking for the perfect rental, our site is tailored to meet your needs.
+            </p>
+            <p className='mb-4 text-slate-700'>
+            At Nest Quest, our mission is to provide personalized services that cater to your unique situation. We believe that every real estate journey is different, which is why we take the time to understand your goals. Whether you’re selling your property for the best possible price, buying your forever home, or finding a rental that fits your lifestyle, we are here to guide you every step of the way. Your satisfaction is our top priority, and we’re committed to making your real estate experience seamless and enjoyable.
+            </p>
+            <p className='mb-4 text-slate-700'>
+            Our team of expert agents brings a wealth of knowledge and industry experience to the table. With in-depth insights into local markets and a passion for helping clients, we are dedicated to providing you with exceptional support. From start to finish, our agents will be by your side, answering your questions and ensuring every detail is handled with care. Trust us to make your real estate journey smooth, efficient, and rewarding. At Nest Quest, we’re here to turn your real estate dreams into reality.
+            </p>
         </div>
     )
 }
